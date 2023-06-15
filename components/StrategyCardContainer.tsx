41341ce7507bff2c68f08bda884b642536849d3f
Usman <usman@arena-ai.com>
2023-06-15 10:01:34 -0400

    animates out card on click of strategy

--- components/StrategyCardContainer.tsx
+++ components/StrategyCardContainer.tsx
@@ -6,6 +6,8 @@ import styles from '../styles/StrategyCardContainer.module.scss';
 import { Image } from '@mantine/core';
 import { sendEvent, MapEvents } from '../utils/events';
 
+import { AnimatePresence, motion, useAnimationControls } from 'framer-motion';
+
 import QuitoIcon from './RegionTiles/Quito.svg';
 import CostaCentroIcon from './RegionTiles/CostaCentro.svg';
 import CostaNorteIcon from './RegionTiles/CostaNorte.svg';
@@ -15,10 +17,41 @@ import SierraCentroIcon from './RegionTiles/SierraCentro.svg';
 import SierraOrienteIcon from './RegionTiles/SierraOriente.svg';
 import SierraSurIcon from './RegionTiles/SierraSur.svg';
 
-
 const StrategyCardContainer = () => {
+    const [shouldAnimateOut, startAnimateOut] = useState(false);
+
+    // setup controls to hide the strategy cards
+    const containerPositionControls = useAnimationControls();
+
+    const containerAnimationPositions = {
+        hide: {
+            right: -600,
+            transition: {
+                type: 'spring',
+                damping: 30,
+                stiffness: 500,
+                restDelta: 0.001,
+            },
+        },
+    };
+
+    const hideStrategyCards = async () => {
+        containerPositionControls.start('hide');
+    };
+
+
+    const onCardClicked = evt => {
+        console.log('clicked');
+        hideStrategyCards();
+        sendEvent(MapEvents.onZoomRequested, 12);
+    };
+
     return (
-        <div className={styles.stratCardContainer}>
+        <motion.div
+            className={styles.stratCardContainer}
+            animate={containerPositionControls}
+            variants={containerAnimationPositions}
+        >
             <Carousel
                 slideSize="415px"
                 // breakpoints={[{ maxWidth: 'sm', slideSize: '100%', slideGap: 2 }]}
@@ -26,14 +59,9 @@ const StrategyCardContainer = () => {
                 align="start"
                 slidesToScroll={1}
                 withControls={false}
-                style={{height: '720px'}}
+                style={{ height: '720px' }}
             >
-                <Carousel.Slide
-                    onClick={e => {
-                        console.log('clicked');
-                        sendEvent(MapEvents.onZoomRequested, 12);
-                    }}
-                >
+                <Carousel.Slide onClick={evt => onCardClicked(evt)}>
                     <StrategyOverviewCard
                         lineGraph={
                             <Image
@@ -47,7 +75,16 @@ const StrategyCardContainer = () => {
                         strategyName="Increase Coverage KitKat All SKUS"
                         goalName={['Increase Coverage', 'Grow Revenue']}
                         productNames="KitKat 12pack"
-                        regionNames={[  QuitoIcon, CostaCentroIcon, CostaNorteIcon, CostaSurIcon, GuayaquilIcon, SierraCentroIcon, SierraOrienteIcon, SierraSurIcon]}
+                        regionNames={[
+                            QuitoIcon,
+                            CostaCentroIcon,
+                            CostaNorteIcon,
+                            CostaSurIcon,
+                            GuayaquilIcon,
+                            SierraCentroIcon,
+                            SierraOrienteIcon,
+                            SierraSurIcon,
+                        ]}
                         leverNames={['BEES Force', 'BEES Grow', 'BEES Customer']}
                     />
                 </Carousel.Slide>
@@ -65,13 +102,17 @@ const StrategyCardContainer = () => {
                         strategyName="Increase Coverage KitKat All SKUS"
                         goalName={['Increase Coverage']}
                         productNames="KitKat 12pack"
-                        regionNames={[  CostaSurIcon, GuayaquilIcon, SierraOrienteIcon, SierraSurIcon]}
+                        regionNames={[
+                            CostaSurIcon,
+                            GuayaquilIcon,
+                            SierraOrienteIcon,
+                            SierraSurIcon,
+                        ]}
                         leverNames={['BEES Force', 'BEES Grow', 'BEES Customer']}
                     />
                 </Carousel.Slide>
-                
             </Carousel>
-        </div>
+        </motion.div>
     );
 };
 
